6b5d19160867517bad06c373dccf3bf2208b981e
Gougougogogo <394442302@qq.com>
2016-08-10 17:13:47 +0800

    AEM-Week2

---
+++ kxi-aem-demo/ui.apps/src/main/content/jcr_root/apps/kxiApps/components/content/banner/banner.js
@@ -0,0 +1,7 @@
+use(function () {
+    return {
+        mainTitle: properties.get("maintext","Main-Tile"),
+        subTitle: properties.get("subtext","Sub-Title"),
+        background: "background: url('/content/xxy/_jcr_content/banner/image1.img.jpeg')" //TODO: Get uploaded picture url,
+    };
+});
