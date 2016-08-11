f62a6aa0928a28ff04dca872d36598c1ca242132
Gougougogogo <394442302@qq.com>
2016-08-11 09:35:36 +0800

    Change img path

--- kxi-aem-demo/ui.apps/src/main/content/jcr_root/apps/kxiApps/components/content/banner/banner.js
+++ kxi-aem-demo/ui.apps/src/main/content/jcr_root/apps/kxiApps/components/content/banner/banner.js
@@ -2,6 +2,9 @@ use(function () {
     return {
         mainTitle: properties.get("maintext","Main-Tile"),
         subTitle: properties.get("subtext","Sub-Title"),
-        background: "background: url('/content/xxy/_jcr_content/banner/image1.img.jpeg')" //TODO: Get uploaded picture url,
+        t1: properties.get("backgroundImg",String.class),
+        t2: properties.get("fileReference",String.class),
+        t3: resource.path + "/" +  properties.get("backgroundImgName",String.class),
+        background: "background: url('/content/xxy/_jcr_content/banner/image1.img.jpeg')" //TODO: Get uploaded picture url
     };
 });
